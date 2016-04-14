f19e1917eb6d2f9fc405571357f395a7a9689a4f
Shawn Horvatic <SH50169@pekininsurance.com>
2016-04-14 13:36:14 -0500

    User input cleaned, and test added

---
+++ TicTacToe/Program.fs
@@ -0,0 +1,17 @@
+﻿open CleanInput
+
+[<EntryPoint>]
+let main argv =
+    let mutable nonLost = true;
+    let mutable cnt = 0;
+    while nonLost do
+        printfn "-----------------------------------------------------------"
+        try
+            cnt <- Sanitize (System.Console.ReadLine())
+            printfn "%i" cnt
+            if cnt > 10 then
+                nonLost <- false
+        with
+            | :? NonIntError -> printfn "Invaild Number"
+    
+    0 // return an integer exit code
