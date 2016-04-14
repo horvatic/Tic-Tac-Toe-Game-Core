78bd3eb22dfb6a68acf2df3aa42ffd1fb665de61
Shawn Horvatic <SH50169@pekininsurance.com>
2016-04-14 14:21:36 -0500

    More test, and make sure user input are in correct block

--- TicTacToe/CleanInput.fs
+++ TicTacToe/CleanInput.fs
@@ -1,10 +1,18 @@
 ﻿module CleanInput
 exception NonIntError of string
+exception OutOfBoundsOverFlow of string
+exception OutOfBoundsUnderFlow of string
 
 let Sanitize ( input : string ) : int32 =
-    let mutable cnt = 0;
+    let mutable userInput = 0;
     try
-            cnt <- int input
+        userInput <- int input
+        if userInput > 9 then
+            raise(OutOfBoundsOverFlow("Value to large"))
+        elif userInput < 1 then
+            raise(OutOfBoundsUnderFlow("Value to small"))
     with
         | :? System.FormatException -> raise(NonIntError("Not an Int"))
-    cnt
+        | :? OutOfBoundsOverFlow -> raise(OutOfBoundsOverFlow("Value to large"))
+        | :? OutOfBoundsUnderFlow -> raise(OutOfBoundsUnderFlow("Value to small"))
+    userInput
