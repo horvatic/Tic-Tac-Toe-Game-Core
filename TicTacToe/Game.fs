c67683d705c879019c4ab89b73bb40b26c92d4c0
Shawn Horvatic <SH50169@pekininsurance.com>
2016-04-18 12:15:20 -0500

    Put screen editing in own file

--- TicTacToe/Game.fs
+++ TicTacToe/Game.fs
@@ -3,17 +3,7 @@ open CleanInput
 open TicTacToeBoxEdit
 open AI
 open CheckForWinnerOrTie
-
-let writeToScreen(ticTacToeBox : array<string>) (message : string) =
-    System.Console.Clear()
-    printfn "%s" message
-    printfn "------------------------"
-    printfn "|  %s   |   %s   |   %s   |" (string ticTacToeBox.[0]) (string ticTacToeBox.[1]) (string ticTacToeBox.[2])
-    printfn "------------------------"
-    printfn "|  %s   |   %s   |   %s   |" (string ticTacToeBox.[3]) (string ticTacToeBox.[4]) (string ticTacToeBox.[5])
-    printfn "------------------------"
-    printfn "|  %s   |   %s   |   %s   |" (string ticTacToeBox.[6]) (string ticTacToeBox.[7]) (string ticTacToeBox.[8])
-    printfn "------------------------"
+open ScreenEdit
 
 let startGame () = 
     let mutable gameNotOver = true
