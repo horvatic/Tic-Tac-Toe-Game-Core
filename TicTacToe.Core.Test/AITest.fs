77461aacc6c1237d1c338543574d0b2c304750e1
Shawn Horvatic <SH50169@pekininsurance.com>
2016-04-28 09:01:46 -0500

    Code Clean Up

--- TicTacToe.Core.Test/AITest.fs
+++ TicTacToe.Core.Test/AITest.fs
@@ -44,16 +44,6 @@ let AI_Block_Wining_Move_3X3() =
                          "X"; "X"; "-9-"|]
     Assert.Equal(8, computerMove ticTacToeBox gameTestCreate)
 
-(*
-[<Fact>] //test
-let AI_Takes_Center_3X3() =
-    let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]) 
-                                          ("X") ("O") (int playerVals.Human)(false)(false)
-    let ticTacToeBox = [|"X"; "-2-"; "-3-"; 
-                         "-4-"; "-5-"; "-6-"; 
-                         "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(4, computerMove ticTacToeBox gameTestCreate)
-*)
 [<Fact>] //test
 let AI_Block_Wining_Move_4X4() =
     let ticTacToeBox = [|"@"; "X"; "X"; "-4-"; 
