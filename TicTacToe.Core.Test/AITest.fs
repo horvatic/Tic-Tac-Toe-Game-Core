f09f30bfbd66fffcc6911aa9863aa7ac8f05e2e1
Shawn Horvatic <SH50169@pekininsurance.com>
2016-05-02 13:17:37 -0500

    Added depth, needs more testing

--- TicTacToe.Core.Test/AITest.fs
+++ TicTacToe.Core.Test/AITest.fs
@@ -9,6 +9,7 @@ open System.Collections.Generic
 open GameSettings
 open InputOutPutTestGame
 
+
 [<Fact>]    // test
 let Ai_Wins_Human_Puting_In_Random_Vals() =
     let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; 
@@ -132,71 +133,3 @@ let Find_Largest_Value() =
     let scores = [|1; -10; 10; 9; 6; -1; 2; 3; 1|]
     Assert.Equal(10, findLargestScore scores )
 
-(*
-let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]) 
-                                          ("X") ("@") (int playerVals.Human)(false)(false)
-
-[<Fact>] //test
-let Get_Box_Legnth_3x3() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(3, getBoxLength ticTacToeBox.Length )
-
-[<Fact>] //test
-let Get_Box_Legnth_4x4() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; 
-                        "-5-"; "-6-"; "-7-"; "-8-"; 
-                        "-9-"; "-10-"; "-11-"; "-12-";
-                        "-13-"; "-14-"; "-15-"; "-16-"|]
-    Assert.Equal(4, getBoxLength ticTacToeBox.Length )
-
-[<Fact>] //test
-let Make_Empty_Tic_Tac_Toe_Box_3X3() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal<string>(ticTacToeBox, makeEmptyTicTacToeBox ticTacToeBox.Length )
-
-[<Fact>] //test
-let Make_Empty_Tic_Tac_Toe_Box_4X4() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; 
-                        "-5-"; "-6-"; "-7-"; "-8-"; 
-                        "-9-"; "-10-"; "-11-"; "-12-";
-                        "-13-"; "-14-"; "-15-"; "-16-"|]
-    Assert.Equal<string>(ticTacToeBox, makeEmptyTicTacToeBox ticTacToeBox.Length )
-
-
-[<Fact>] //test
-let Make_Empty_Score_3X3() =
-    let scores = [|0; 0; 0; 0; 0; 0; 0; 0; 0|]
-    Assert.Equal<int>(scores, makeEmptyScore scores.Length )
-
-[<Fact>] //test
-let Make_Empty_Score_4X4() =
-    let scores = [|0; 0; 0; 0; 
-                        0; 0; 0; 0; 
-                        0; 0; 0; 0;
-                        0; 0; 0; 0|]
-    Assert.Equal<int>(scores, makeEmptyScore scores.Length )
-
-[<Fact>] //test
-let Test_Scan_For_Already_Made_Trees() =
-    let oldTrees = new Dictionary<string, int>()
-    oldTrees.Add("XO3456789", -10)
-    oldTrees.Add("XO3X56789", -5)
-    Assert.Equal(-10, checkForAlreadyMadeTree oldTrees "XO3456789" )
-
-[<Fact>] //test
-let Test_Scan_For_NoValue_In_Already_Made_Trees() =
-    let oldTrees = new Dictionary<string, int>()
-    oldTrees.Add("XO3456789", -10)
-    Assert.Equal(-9999, checkForAlreadyMadeTree oldTrees "X23456789" )
-
-[<Fact>] //test
-let Test_Scan_For_NoValue_In_None_Made_Trees() =
-    let oldTrees = new Dictionary<string, int>()
-    Assert.Equal(-9999, checkForAlreadyMadeTree oldTrees "X23456789" )
-
-[<Fact>] //test
-let TicTacToeBox_To_String() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal("-1--2--3--4--5--6--7--8--9-", 
-        ticTacToeBoxToString ticTacToeBox )
-*)
--- TicTacToe/AI.fs
+++ TicTacToe/AI.fs
@@ -5,151 +5,6 @@ open GameStatusCodes
 open System.Collections.Generic
 open GameSettings
 
-(*
-let ticTacToeBoxToString( ticTacToeBox : array<string>) : string =
-    ticTacToeBox |> String.concat ""
-
-let checkForAlreadyMadeTree( oldTrees : Dictionary<string, int> )
-                           (key : string ) : int =
-    if oldTrees.ContainsKey(key) then
-        let returnScore = ref 0
-        if oldTrees.TryGetValue(key, returnScore) then
-            returnScore.Value
-        else
-            -9999
-    else
-        -9999
-    
-
-let getBoxLength( arrayLength : int ) : int =
-    let length = sqrt( float arrayLength )
-    int length
-
-let makeEmptyScore ( length : int ) : array<int> =
-    if length = 9 then
-        let scoces = [|0; 0; 0; 0; 0; 0; 0; 0; 0|]
-        scoces
-    else
-        let scoces = [|0; 0; 0; 0; 
-                       0; 0; 0; 0;
-                       0; 0; 0; 0;
-                       0; 0; 0; 0;|]
-        scoces 
-
-let makeEmptyTicTacToeBox( length : int ) : array<string> =
-    if length = 9 then
-        let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-        ticTacToeBox
-    else
-        let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; 
-                        "-5-"; "-6-"; "-7-"; "-8-"; 
-                        "-9-"; "-10-"; "-11-"; "-12-";
-                        "-13-"; "-14-"; "-15-"; "-16-"|]
-        ticTacToeBox
-
-let rec minimax( ticTacToeBox : array<string>)(player : int)
-               (oldTrees : Dictionary<string, int>)(game : gameSetting)
-               ( depth : int )
-             : int =
-    
-    let boxSide = getBoxLength(ticTacToeBox.Length)
-    let mutable currentPlayer = player
-    let mutable score = 0
-    let mutable moves = makeEmptyTicTacToeBox(ticTacToeBox.Length)
-    
-    for i = 0 to ticTacToeBox.Length - 1 do
-        moves.[i] <- ticTacToeBox.[i]
-
-    score <- checkForAlreadyMadeTree(oldTrees) 
-            (ticTacToeBoxToString(moves))
-    if score = -9999 then
-        score <- checkForWinnerOrTie(moves)(game.playerGlyph)(game.aIGlyph)
-    
-    if score = int GenResult.NoWinner then
-        let mutable scores = makeEmptyScore(ticTacToeBox.Length)
-        for i = 0 to ticTacToeBox.Length - 1 do
-            if (not (moves.[i] = game.playerGlyph || moves.[i] = game.aIGlyph)) then
-                if currentPlayer = int playerVals.AI then
-                    moves.[i] <- game.aIGlyph
-                    scores.[i] <- minimax(moves)(currentPlayer * -1)(oldTrees)(game)(depth + 1)
-                    if not (oldTrees.ContainsKey(ticTacToeBoxToString(moves))) then
-                        oldTrees.Add(ticTacToeBoxToString(moves), scores.[i])
-                else
-                    moves.[i] <- game.playerGlyph
-                    scores.[i] <- minimax(moves)(currentPlayer * -1)(oldTrees)(game)(depth + 1)
-                    if not (oldTrees.ContainsKey(ticTacToeBoxToString(moves))) then
-                        oldTrees.Add(ticTacToeBoxToString(moves), scores.[i])
-                moves.[i] <- string (i+1)
-            
-        if currentPlayer = int playerVals.AI then
-            let mutable bestScore = -999
-            let mutable place = -1
-            for i = 0 to ticTacToeBox.Length - 1 do
-                if bestScore < scores.[i] && not (moves.[i] = game.playerGlyph || moves.[i] = game.aIGlyph) then
-                    bestScore <- scores.[i]
-                    score <- bestScore
-                    place <- i
-            moves.[place] <- game.aIGlyph
-            
-        else
-            let mutable bestScore = 999
-            let mutable place = -1
-            for i = 0 to ticTacToeBox.Length - 1 do
-                if bestScore > scores.[i] && not (moves.[i] = game.playerGlyph || moves.[i] = game.aIGlyph) then
-                    bestScore <- scores.[i]
-                    score <- bestScore
-                    place <- i
-            moves.[place] <- game.playerGlyph
-   
-    currentPlayer <- currentPlayer * -1
-    if currentPlayer = int playerVals.AI then
-        score <- (score - depth)
-    else
-        score <- ( depth + score )
-    score
-
-let movesCount ( ticTacToeBox : array<string>)(game : gameSetting) : int =
-    let mutable count = 0
-    for i = 0 to ticTacToeBox.Length - 1 do
-        if ticTacToeBox.[i] = game.playerGlyph || ticTacToeBox.[i] = game.aIGlyph then
-            count <- count + 1
-    count
-
-let moveHere( ticTacToeBox : array<string>)(game : gameSetting)
-             : int =
-    let mutable place = -1
-    let mutable i = 0
-    while place = -1 do
-        if not (ticTacToeBox.[i] = game.playerGlyph || ticTacToeBox.[i] = game.aIGlyph) then
-            place <- i
-        else
-            i <- i + 1
-    place
-
-let computerMove(game : gameSetting)
-             : int =
-
-    if movesCount(game.ticTacToeBox)(game) < 2 && game.ticTacToeBox.Length = 16 then
-        moveHere(game.ticTacToeBox)(game)
-    else
-        let mutable place = -1
-        let oldTrees = new Dictionary<string, int>()
-        let mutable scores = makeEmptyScore(game.ticTacToeBox.Length)
-        for i = 0 to game.ticTacToeBox.Length - 1 do
-            if (not (game.ticTacToeBox.[i] = game.playerGlyph || game.ticTacToeBox.[i] = game.aIGlyph)) then
-                game.ticTacToeBox.[i] <- game.aIGlyph
-                scores.[i] <- minimax(game.ticTacToeBox)(int playerVals.Human)(oldTrees)(game)(1)
-                let stringOffset = i + 1
-                game.ticTacToeBox.[i] <- string ("-"+string stringOffset+"-")
-    
-        let mutable bestScore = -999
-        for i = 0 to game.ticTacToeBox.Length - 1 do
-            if bestScore < scores.[i] && not (game.ticTacToeBox.[i] = game.playerGlyph || game.ticTacToeBox.[i] = game.aIGlyph) then
-                bestScore <- scores.[i]
-                place <- i
-        place
-*)
-
 let insertPlayerGlyph(ticTacToeBox : array<string>)
                      (insetPostion : int)
                      (playerGlyph : string)
@@ -172,21 +27,21 @@ let insertAiGlyph(ticTacToeBox : array<string>)
 
 let makeEmptyScoreMax ( length : int ) : array<int> =
     if length = 9 then
-        [|0; 0; 0; 0; 0; 0; 0; 0; 0|]
+        [|999; 999; 999; 999; 999; 999; 999; 999; 999|]
     else
-        [|0; 0; 0; 0; 
-        0; 0; 0; 0;
-        0; 0; 0; 0;
-        0; 0; 0; 0;|]
+        [|999; 999; 999; 999; 
+        999; 999; 999; 999;
+        999; 999; 999; 999;
+        999; 999; 999; 999;|]
 
 let makeEmptyScoreMin ( length : int ) : array<int> =
     if length = 9 then
-        [|-0; -0; -0; -0; -0; -0; -0; -0; -0|]
+        [|-999; -999; -999; -999; -999; -999; -999; -999; -999|]
     else
-        [|-0; -0; -0; -0; 
-        -0; -0; -0; -0;
-        -0; -0; -0; -0;
-        -0; -0; -0; -0;|]
+        [|-999; -999; -999; -999; 
+        -999; -999; -999; -999;
+        -999; -999; -999; -999;
+        -999; -999; -999; -999;|]
 
 let makeDeepCopyOfArray(copyTo : array<string>)
                        (copyFrom : array<string> ) 
@@ -226,9 +81,9 @@ let rec miniMaxMaxium(ticTacToeBox : array<string>)
             if (insertAiGlyph(moves)(i)(playerGlyph)(aIGlyph)) then
                 scores.[i] <- miniMaxMinimize(moves)(playerGlyph)(aIGlyph)(depth + 1)
                 moves.[i] <- "-"+string (int i + int 1)+"-"
-        findLargestScore(scores)
+        findLargestScore(scores) - depth
     else
-        score + depth
+        score
 
 and miniMaxMinimize(ticTacToeBox : array<string>)
                    (playerGlyph : string)
@@ -244,9 +99,9 @@ and miniMaxMinimize(ticTacToeBox : array<string>)
             if (insertPlayerGlyph(moves)(i)(playerGlyph)(aIGlyph)) then
                 scores.[i] <- miniMaxMaxium(moves)(playerGlyph)(aIGlyph)(depth + 1)
                 moves.[i] <- "-"+string (int i + int 1)+"-"
-        findSmallestScore(scores)
+        findSmallestScore(scores) + depth
     else
-        score - depth
+        score
 
 let computerMove(ticTacToeBox : array<string>)
                 (playerGlyph : string)
