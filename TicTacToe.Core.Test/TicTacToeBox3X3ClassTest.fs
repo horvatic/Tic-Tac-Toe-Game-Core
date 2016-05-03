2328c171aaafdc99abbc1c34f818c8ad742c0c67
Shawn Horvatic <SH50169@pekininsurance.com>
2016-05-03 12:55:52 -0500

    Added layout of abstracting TicTacToeBox

---
+++ TicTacToe.Core.Test/TicTacToeBox3X3ClassTest.fs
@@ -0,0 +1,69 @@
+﻿module TicTacToeBox3X3ClassTest
+open TicTacToeBox3X3Class
+open Xunit 
+open FsUnit
+
+[<Fact>]
+let Create_New_TicTacToe_Object() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    Assert.NotNull(ticTacToeBoard)
+
+[<Fact>]
+let Check_Cell_Count_Is_9() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    Assert.Equal(9, ticTacToeBoard.cellCount() )
+
+[<Fact>]
+let Victory_Cell_Count_Is_3() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    Assert.Equal(3, ticTacToeBoard.victoryCellCount() )
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(10)>]
+let Index_Out_Of_Bounds_Get_Cell_Glyph(position : int) =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    (fun() -> ticTacToeBoard.getCellGlyph position |> ignore ) |> should throw typeof<IndexOutOfBoundsException>
+
+[<Theory>]
+[<InlineData(1)>]
+[<InlineData(5)>]
+[<InlineData(9)>]
+let Get_Cell_Glyph_On_A_New_Board(position : int) =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    Assert.Equal("", ticTacToeBoard.getCellGlyph position)
+
+[<Theory>]
+[<InlineData(0)>]
+[<InlineData(10)>]
+let Index_Out_Of_Bounds_Insert_Cell_Glyph_At(position : int) =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    (fun() -> ticTacToeBoard.insertCellGlyphAt(position, "X") |> ignore ) |> should throw typeof<IndexOutOfBoundsException>
+
+[<Theory>]
+[<InlineData(1)>]
+[<InlineData(5)>]
+[<InlineData(9)>]
+let Insert_Sucessful_In_Tic_Tac_Toe_Box(position : int) =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    ticTacToeBoard.insertCellGlyphAt(position, "X")
+    Assert.Equal("X", ticTacToeBoard.getCellGlyph(position))
+
+[<Fact>]
+let Spot_Already_Taken_Same_Player() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    ticTacToeBoard.insertCellGlyphAt(1, "X")
+    (fun() -> ticTacToeBoard.insertCellGlyphAt(1, "X") |> ignore ) |> should throw typeof<SpotTakenException>
+
+[<Fact>]
+let Spot_Already_Taken_Differnt_Player() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    ticTacToeBoard.insertCellGlyphAt(1, "X")
+    (fun() -> ticTacToeBoard.insertCellGlyphAt(1, "O") |> ignore ) |> should throw typeof<SpotTakenException>
+
+[<Fact>]
+let Clone_Tic_Tac_Toe_Obejct_Sucessful() =
+    let ticTacToeBoard = new TicTacToeBox3X3()
+    ticTacToeBoard.insertCellGlyphAt(1, "X")
+    let clonedTicTacToeBoard = ticTacToeBoard.clone() :?> TicTacToeBox3X3
+    Assert.Equal("X", clonedTicTacToeBoard.getCellGlyph(1))
