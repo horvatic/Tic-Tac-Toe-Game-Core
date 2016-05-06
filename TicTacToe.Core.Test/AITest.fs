94369f5fd1745635aa2347f1c178ac16090543f4
Shawn Horvatic <SH50169@pekininsurance.com>
2016-05-06 15:10:14 -0500

    Ai mutable all removed

--- TicTacToe.Core.Test/AITest.fs
+++ TicTacToe.Core.Test/AITest.fs
@@ -9,62 +9,43 @@ open System.Collections.Generic
 open GameSettings
 open InputOutPutTestGame
 
-[<Fact>]
-let Edit_Tic_Tac_Toe_Box() =
-    let ticTacToeBox = ["-1-"; "-2-"; "-3-"; 
-                        "-4-"; "-5-"; "-6-"; 
-                        "-7-"; "-8-"; "-9-"]
-    let ticTacToeBoxEdited = ["-1-"; "-2-"; "-3-"; 
-                            "-4-"; "-5-"; "-6-"; 
-                            "-7-"; "X"; "-9-"]
-    Assert.Equal<string>(ticTacToeBoxEdited, getEditedTicTacToeBox ticTacToeBox 7 "X")
+let rec humanAndAIPlay3X3(ticTacToeBox : list<string>)
+                       (playerGlyph : string)
+                       (aIGlyph : string)
+                       (game : gameSetting)  =
+    
+    let game = craftGameSetting (ticTacToeBox |> List.toArray) 
+                                (playerGlyph) (aIGlyph) 
+                                (int playerVals.Human)(false)(false)(false)
+    
+    if checkForWinnerOrTie(game.ticTacToeBox)(playerGlyph)(aIGlyph)  = int GenResult.NoWinner then
+        aIMove(game)
+        if checkForWinnerOrTie(game.ticTacToeBox)(playerGlyph)(aIGlyph)  = int GenResult.NoWinner then
+            for i = 0 to game.ticTacToeBox.Length - 1 do
+                if not(game.ticTacToeBox.[i] = playerGlyph || game.ticTacToeBox.[i] = aIGlyph) then 
+                    humanAndAIPlay3X3((getEditedTicTacToeBox(game.ticTacToeBox |> Array.toList)(i)(playerGlyph)))
+                                     (playerGlyph)(aIGlyph)(game)
+        else
+            Assert.NotEqual(checkForWinnerOrTie(game.ticTacToeBox)
+                                               (playerGlyph)
+                                               (aIGlyph), int Result3X3.HumanWins)
+    else
+        Assert.NotEqual(checkForWinnerOrTie(game.ticTacToeBox)
+                                           (playerGlyph)
+                                           (aIGlyph), int Result3X3.HumanWins)
 
-(*
-let rec humanAndAIPlay4X4(ticTacToeBox : array<string>)
-                       (playerGlyph : string)
-                       (aIGlyph : string)
-                       (game : gameSetting)  =
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(ticTacToeBox.Length))(ticTacToeBox)
-    let game = craftGameSetting (moves) 
-                                (playerGlyph) (aIGlyph) 
-                                (int playerVals.Human)(false)(false)(false)
-    let mutable score = checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
-    if score = int GenResult.NoWinner then
-        aIMove(game)
-        score <- checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
-        if score = int GenResult.NoWinner then
-            for i = 0 to moves.Length - 1 do
-            if (insertPlayerGlyph(moves)(i)("X")("@")) then
-                humanAndAIPlay4X4(moves)("X")("@")(game) |> ignore
-                moves.[i] <- "-"+string (int i + int 1)+"-"
-        else
-            Assert.NotEqual(score, int Result4X4.HumanWins)
-    else
-        Assert.NotEqual(score, int Result4X4.HumanWins)
-*)
-let rec humanAndAIPlay3X3(ticTacToeBox : array<string>)
-                       (playerGlyph : string)
-                       (aIGlyph : string)
-                       (game : gameSetting)  =
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(ticTacToeBox.Length))(ticTacToeBox)
-    let game = craftGameSetting (moves) 
-                                (playerGlyph) (aIGlyph) 
-                                (int playerVals.Human)(false)(false)(false)
-    let mutable score = checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
-    if score = int GenResult.NoWinner then
-        aIMove(game)
-        score <- checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
-        if score = int GenResult.NoWinner then
-            for i = 0 to moves.Length - 1 do
-            if (insertPlayerGlyph(moves)(i)(playerGlyph)(aIGlyph)) then
-                humanAndAIPlay3X3(moves)(playerGlyph)(aIGlyph)(game) |> ignore
-                moves.[i] <- "-"+string (int i + int 1)+"-"
-        else
-            Assert.NotEqual(score, int Result3X3.HumanWins)
-    else
-        Assert.NotEqual(score, int Result3X3.HumanWins)
+[<Fact>]
+let Evey_Possabile_Combination_Game_3X3_Huamn_First() =
+    let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; 
+                                             "-4-"; "-5-"; "-6-"; 
+                                             "-7-"; "-8-"; "-9-"|]) 
+                                          ("X") ("@") 
+                                          (int playerVals.Human)(false)(false)(false)
+    let ticTacToeBox = gameTestCreate.ticTacToeBox |> Array.toList
+    for i = 0 to ticTacToeBox.Length - 1 do
+        if not(ticTacToeBox.[i] = gameTestCreate.playerGlyph || ticTacToeBox.[i] = gameTestCreate.aIGlyph) then
+            humanAndAIPlay3X3(getEditedTicTacToeBox(ticTacToeBox)(i)(gameTestCreate.playerGlyph))
+                             (gameTestCreate.playerGlyph)(gameTestCreate.aIGlyph)(gameTestCreate)
 
 [<Fact>]
 let Evey_Possabile_Combination_Game_3X3_AI_First() =
@@ -74,43 +55,12 @@ let Evey_Possabile_Combination_Game_3X3_AI_First() =
                                           ("X") ("@") 
                                           (int playerVals.Human)(false)(false)(false)
     aIMove(gameTestCreate)
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(gameTestCreate.ticTacToeBox.Length))(gameTestCreate.ticTacToeBox)
-    for i = 0 to moves.Length - 1 do
-        if (insertPlayerGlyph(moves)(i)("X")("@")) then
-            humanAndAIPlay3X3(moves)("X")("@")(gameTestCreate)|> ignore
-            moves.[i] <- "-"+string (int i + int 1)+"-"
+    let ticTacToeBox = gameTestCreate.ticTacToeBox |> Array.toList
+    for i = 0 to ticTacToeBox.Length - 1 do
+        if not(ticTacToeBox.[i] = gameTestCreate.playerGlyph || ticTacToeBox.[i] = gameTestCreate.aIGlyph) then
+            humanAndAIPlay3X3(getEditedTicTacToeBox(ticTacToeBox)(i)(gameTestCreate.playerGlyph))
+                             (gameTestCreate.playerGlyph)(gameTestCreate.aIGlyph)(gameTestCreate)
 
-(*
-[<Fact>]
-let Evey_Possabile_Combination_Game_4X4_AI_First() =
-    let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; "-4-"; 
-                                            "-5-"; "-6-"; "-7-"; "-8-"; 
-                                            "-9-"; "-10-"; "-11-"; "-12-";
-                                            "-13-"; "-14-"; "-15-"; "-16-"|]) 
-                                          ("X") ("@") 
-                                          (int playerVals.Human)(false)(false)(false)
-    aIMove(gameTestCreate)
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(gameTestCreate.ticTacToeBox.Length))(gameTestCreate.ticTacToeBox)
-    for i = 0 to moves.Length - 1 do
-        if (insertPlayerGlyph(moves)(i)("X")("@")) then
-            humanAndAIPlay4X4(moves)("X")("@")(gameTestCreate)|> ignore
-            moves.[i] <- "-"+string (int i + int 1)+"-"
-*)
-[<Fact>]
-let Evey_Possabile_Combination_Game_3X3_Huamn_First() =
-    let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; 
-                                             "-4-"; "-5-"; "-6-"; 
-                                             "-7-"; "-8-"; "-9-"|]) 
-                                          ("X") ("@") 
-                                          (int playerVals.Human)(false)(false)(false)
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(gameTestCreate.ticTacToeBox.Length))(gameTestCreate.ticTacToeBox)
-    for i = 0 to moves.Length - 1 do
-        if (insertPlayerGlyph(moves)(i)("X")("@")) then
-            humanAndAIPlay3X3(moves)("X")("@")(gameTestCreate)|> ignore
-            moves.[i] <- "-"+string (int i + int 1)+"-"
 
 [<Fact>]
 let Maxiume_Lossess() = 
@@ -167,16 +117,6 @@ let Ai_Wins_Human_Puting_In_Order_Vals() =
     Assert.Equal(getWinningAIValue(gameTestCreate.ticTacToeBox)
                     , winningVaue)
 
-[<Fact>] 
-let Insert__Player_Glyph_Sucessfuly() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(true, insertPlayerGlyph ticTacToeBox 0 "X" "@")
-
-[<Fact>] 
-let Insert__Player_Glyph_unSucessfuly_With_AI() =
-    let ticTacToeBox = [|"@"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(false, insertPlayerGlyph ticTacToeBox 0 "X" "@")
-
 [<Fact>]
 let Mini_Max_Minimize_Win() = 
     let gameTestCreate = craftGameSetting ([|"@"; "@"; "-3-"; "-4-"; "-5-"; "-6-"; "X"; "X"; "-9-"|]) 
@@ -230,77 +170,54 @@ let Mini_Max_Minimize_Tie() =
                                     gameTestCreate.aIGlyph
                                     0)
 
-
-[<Fact>] 
-let Insert__AI_Glyph_Sucessfuly() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(true, insertAiGlyph ticTacToeBox 0 "X" "@")
-
-[<Fact>] 
-let Insert__AI_Glyph_unSucessfuly_With_AI() =
-    let ticTacToeBox = [|"@"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(false, insertAiGlyph ticTacToeBox 0 "X" "@")
-
-[<Fact>] 
-let Insert__AI_Glyph_unSucessfuly_With_Human() =
-    let ticTacToeBox = [|"X"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal(false, insertAiGlyph ticTacToeBox 0 "X" "@")
-
-[<Fact>] //test
-let Make_Empty_Score_Min_3X3() =
-    let scores = [|-999; -999; -999; -999; -999; -999; -999; -999; -999|]
-    Assert.Equal<int>(scores, makeEmptyScoreMin scores.Length )
-
-[<Fact>] //test
-let Make_Empty_Score_Min_4X4() =
-    let scores = [|-999; -999; -999; -999; 
-                    -999; -999; -999; -999; 
-                    -999; -999; -999; -999;
-                    -999; -999; -999; -999|]
-    Assert.Equal<int>(scores, makeEmptyScoreMin scores.Length )
-
-[<Fact>] //test
-let Make_Empty_Score_Max_3X3() =
-    let scores = [|999; 999; 999; 999; 999; 999; 999; 999; 999|]
-    Assert.Equal<int>(scores, makeEmptyScoreMax scores.Length )
-
-[<Fact>] //test
-let Make_Empty_Score_Max_4X4() =
-    let scores = [|999; 999; 999; 999; 
-                    999; 999; 999; 999; 
-                    999; 999; 999; 999;
-                    999; 999; 999; 999|]
-    Assert.Equal<int>(scores, makeEmptyScoreMax scores.Length )
-
 [<Fact>]
-let Make_Deep_Copy_Of_TicTacToeBox() =
-    let ticTacToeBoxCopy = [|"x"; "x"; "-3-"; "@"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-
-    Assert.Equal<string>(ticTacToeBoxCopy, makeDeepCopyOfArray ticTacToeBox ticTacToeBoxCopy)
-[<Fact>] //test
-let Make_Empty_Tic_Tac_Toe_Box_3X3() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    Assert.Equal<string>(ticTacToeBox, makeEmptyTicTacToeBox ticTacToeBox.Length )
-
-[<Fact>] //test
-let Make_Empty_Tic_Tac_Toe_Box_4X4() =
-    let ticTacToeBox = [|"-1-"; "-2-"; "-3-"; "-4-"; 
-                        "-5-"; "-6-"; "-7-"; "-8-"; 
-                        "-9-"; "-10-"; "-11-"; "-12-";
-                        "-13-"; "-14-"; "-15-"; "-16-"|]
-    Assert.Equal<string>(ticTacToeBox, makeEmptyTicTacToeBox ticTacToeBox.Length )
+let Edit_Tic_Tac_Toe_Box() =
+    let ticTacToeBox = ["-1-"; "-2-"; "-3-"; 
+                        "-4-"; "-5-"; "-6-"; 
+                        "-7-"; "-8-"; "-9-"]
+    let ticTacToeBoxEdited = ["-1-"; "-2-"; "-3-"; 
+                            "-4-"; "-5-"; "-6-"; 
+                            "-7-"; "X"; "-9-"]
+    Assert.Equal<string>(ticTacToeBoxEdited, getEditedTicTacToeBox ticTacToeBox 7 "X")
 
 
 (*
-[<Fact>] //test
-let Find_Smallest_Value() =
-    let scores = [1; -10; 10; 9; 6; -1; 2; 3; 1]
-    Assert.Equal(-10, findSmallestScore scores )
+let rec humanAndAIPlay4X4(ticTacToeBox : array<string>)
+                       (playerGlyph : string)
+                       (aIGlyph : string)
+                       (game : gameSetting)  =
+    let moves =
+        makeDeepCopyOfArray(makeEmptyTicTacToeBox(ticTacToeBox.Length))(ticTacToeBox)
+    let game = craftGameSetting (moves) 
+                                (playerGlyph) (aIGlyph) 
+                                (int playerVals.Human)(false)(false)(false)
+    let mutable score = checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
+    if score = int GenResult.NoWinner then
+        aIMove(game)
+        score <- checkForWinnerOrTie(moves)(playerGlyph)(aIGlyph) 
+        if score = int GenResult.NoWinner then
+            for i = 0 to moves.Length - 1 do
+            if (insertPlayerGlyph(moves)(i)("X")("@")) then
+                humanAndAIPlay4X4(moves)("X")("@")(game) |> ignore
+                moves.[i] <- "-"+string (int i + int 1)+"-"
+        else
+            Assert.NotEqual(score, int Result4X4.HumanWins)
+    else
+        Assert.NotEqual(score, int Result4X4.HumanWins)
 
-[<Fact>] //test
-let Find_Largest_Value() =
-    let scores = [1; -10; 10; 9; 6; -1; 2; 3; 1]
-    Assert.Equal(10, findLargestScore scores )
-
+[<Fact>]
+let Evey_Possabile_Combination_Game_4X4_AI_First() =
+    let gameTestCreate = craftGameSetting ([|"-1-"; "-2-"; "-3-"; "-4-"; 
+                                            "-5-"; "-6-"; "-7-"; "-8-"; 
+                                            "-9-"; "-10-"; "-11-"; "-12-";
+                                            "-13-"; "-14-"; "-15-"; "-16-"|]) 
+                                          ("X") ("@") 
+                                          (int playerVals.Human)(false)(false)(false)
+    aIMove(gameTestCreate)
+    let moves =
+        makeDeepCopyOfArray(makeEmptyTicTacToeBox(gameTestCreate.ticTacToeBox.Length))(gameTestCreate.ticTacToeBox)
+    for i = 0 to moves.Length - 1 do
+        if (insertPlayerGlyph(moves)(i)("X")("@")) then
+            humanAndAIPlay4X4(moves)("X")("@")(gameTestCreate)|> ignore
+            moves.[i] <- "-"+string (int i + int 1)+"-"
 *)
--- TicTacToe/AI.fs
+++ TicTacToe/AI.fs
@@ -5,65 +5,6 @@ open GameStatusCodes
 open System.Collections.Generic
 open GameSettings
 
-//***************************************TEST CODE REMOVE IN REFACOR*************************
-let insertPlayerGlyph(ticTacToeBox : array<string>)
-                     (insetPostion : int)
-                     (playerGlyph : string)
-                     (aIGlyph : string) : bool =
-    if ticTacToeBox.[insetPostion] = playerGlyph || ticTacToeBox.[insetPostion] = aIGlyph then
-        false
-    else
-        ticTacToeBox.[insetPostion] <- playerGlyph
-        true
-
-let insertAiGlyph(ticTacToeBox : array<string>)
-                 (insetPostion : int)
-                 (playerGlyph : string)
-                 (aIGlyph : string) : bool =
-    if ticTacToeBox.[insetPostion] = playerGlyph || ticTacToeBox.[insetPostion] = aIGlyph then
-        false
-    else
-        ticTacToeBox.[insetPostion] <- aIGlyph
-        true
-
-let makeEmptyScoreMax ( length : int ) : array<int> =
-    if length = 9 then
-        [|999; 999; 999; 999; 999; 999; 999; 999; 999|]
-    else
-        [|999; 999; 999; 999; 
-        999; 999; 999; 999;
-        999; 999; 999; 999;
-        999; 999; 999; 999;|]
-
-let makeEmptyScoreMin ( length : int ) : array<int> =
-    if length = 9 then
-        [|-999; -999; -999; -999; -999; -999; -999; -999; -999|]
-    else
-        [|-999; -999; -999; -999; 
-        -999; -999; -999; -999;
-        -999; -999; -999; -999;
-        -999; -999; -999; -999;|]
-
-let makeDeepCopyOfArray(copyTo : array<string>)
-                       (copyFrom : array<string> ) 
-                       : array<string> = 
-    for i = 0 to copyFrom.Length - 1 do
-        copyTo.[i] <- copyFrom.[i]
-    copyTo
-
-let makeEmptyTicTacToeBox( length : int ) : array<string> =
-    if length = 9 then
-        [|"-1-"; "-2-"; "-3-"; "-4-"; "-5-"; "-6-"; "-7-"; "-8-"; "-9-"|]
-    else
-        [|"-1-"; "-2-"; "-3-"; "-4-"; 
-        "-5-"; "-6-"; "-7-"; "-8-"; 
-        "-9-"; "-10-"; "-11-"; "-12-";
-        "-13-"; "-14-"; "-15-"; "-16-"|]
-//**************************************************************************************************
-
-
-
-
 let maxiumeScoreCutOff(score : int)(depth : int ) : int =
     if score - depth < int GenResult.Tie then
         int GenResult.Tie
@@ -205,24 +146,7 @@ let computerMove(ticTacToeBox : list<string>)
                 : int =
     
     (computingMove(ticTacToeBox)(playerGlyph)(aIGlyph)([-999;-1])(0)).[1]
-    
-    
-    (*
-    let tempTicTacToeBox = ticTacToeBox |> List.toArray
-    let moves =
-        makeDeepCopyOfArray(makeEmptyTicTacToeBox(ticTacToeBox.Length))(tempTicTacToeBox)
-    let scores = makeEmptyScoreMin(moves.Length)
-    let mutable largestScore = -999
-    let mutable pos = -1
-    for i = 0 to moves.Length - 1 do
-        if (insertAiGlyph(moves)(i)(playerGlyph)(aIGlyph)) then
-            scores.[i] <- miniMaxMinimize(moves |> Array.toList)(playerGlyph)(aIGlyph)(0)
-            moves.[i] <- "-"+string (int i + int 1)+"-"
-            if scores.[i] > largestScore then
-                largestScore <- scores.[i]
-                pos <- i
-    pos
-    *)
+
 
 let aIMove(game : gameSetting) =
     game.ticTacToeBox.[computerMove(game.ticTacToeBox |> Array.toList)(game.playerGlyph)(game.aIGlyph)] <- game.aIGlyph
