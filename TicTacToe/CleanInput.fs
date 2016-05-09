0b76064903b14d339905be5f11d19367fcd7c61e
Shawn Horvatic <SH50169@pekininsurance.com>
2016-05-09 15:08:23 -0500

    Removed all mutablbiity

--- TicTacToe/CleanInput.fs
+++ TicTacToe/CleanInput.fs
@@ -3,40 +3,44 @@ open userInputException
 exception NonIntError of string
 exception InvaildOption of string
 
-let SanitizeHumanPickedPlace ( input : string ) ( textBoxSize : int ) : int32 =
-    let mutable userInput = 0
-    try
-        userInput <- int input
-    with
-        | :? System.FormatException -> raise(NonIntError("Not an Int"))
-    if userInput > textBoxSize then
+let inputWithinBoundsOfTiCTacToeBox ( userInput : int ) ( boxSize : int ) : int32 =
+    if userInput > boxSize then
         raise(OutOfBoundsOverFlow("Value to large"))
     elif userInput < 1 then
         raise(OutOfBoundsUnderFlow("Value to small"))
     userInput
+
+let SanitizeHumanPickedPlace ( input : string ) ( boxSize : int ) : int32 =
+    try
+        let userInput = int input
+        inputWithinBoundsOfTiCTacToeBox(userInput)(boxSize)
+    with
+        | :? System.FormatException -> raise(NonIntError("Not an Int"))
+
 
 let SanitizeGlyph( input : string) : string =
     if input.Length = 1 then
         input
     else
         raise(InvaildGlyph("Length Must be one"))
-
-let SanitizeBoxSize(input : string) : int =
-    let mutable userInput = 0
-    try
-        userInput <- int input
-    with
-        | :? System.FormatException -> raise(NonIntError("Not an Int"))
+ 
+let boxSizeWithinBound(userInput : int ) : int =
     if userInput > 4 then
         raise(OutOfBoundsOverFlow("Value to large"))
     elif userInput < 3 then
         raise(OutOfBoundsUnderFlow("Value to small"))
-
     userInput
+
+let SanitizeBoxSize(input : string) : int =
+    try
+        let userInput = int input
+        boxSizeWithinBound(userInput)
+    with
+        | :? System.FormatException -> raise(NonIntError("Not an Int"))
+
 
 let SanitizeYesOrNo(input : string) : string =
-    let mutable userInput = input
-    userInput <- userInput.ToUpper()
-    if not( userInput = "Y") && not( userInput = "N" ) then
+    let userInput = input
+    if not( userInput.ToUpper() = "Y") && not( userInput.ToUpper() = "N" ) then
         raise(InvaildOption("Must be a Y or N"))
-    userInput
+    userInput.ToUpper()
